872af8323661fa1079859c7e6fe22090e3815797
Lucas <1648136557@qq.com>
2024-07-09 00:56:02 +0800

    修改包名

--- createTemplate/CreateTemplateFactory.go
+++ createTemplate/CreateTemplateFactory.go
@@ -1,9 +1,10 @@
 package createTemplate
 
 import (
-	"github.com/SleepingLucas/ctb/CreateTemplate/impl"
+	"github.com/SleepingLucas/ctb/createTemplate/impl"
 )
 
+// CreateTemplateFactory 创建代码模板工厂
 func CreateTemplateFactory(templateType, problem, url string) CreateTemplate {
 	switch templateType {
 	case "cf":
